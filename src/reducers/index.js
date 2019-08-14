c4e2a91823d95a5b18f893fe15717bb8ca17499a
alekseibykov <gogohandz@gmail.com>
2019-08-14 14:40:48 +0300

    Clean up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux';
 
 import { tasksReducer } from './tasksReducer';
 import { datesReducer } from './datesReducer';
-import { itemReducer } from './itemReducer';
 import sessionReducer from './sessionReducer';
-import userReducer from './userReducer';
 
 export default combineReducers({
   data: tasksReducer,
   dates: datesReducer,
-  show: itemReducer,
   sessionState: sessionReducer,
-  userState: userReducer,
 });
