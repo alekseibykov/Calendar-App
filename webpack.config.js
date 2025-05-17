66556aa8854ff8e7b085e950b2c64251cf7d18e0
Aleksei Bykov <gogohandz@gmail.com>
2025-05-17 21:38:09 +0300

    optimize webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,64 @@
 const path = require("path");
 const Dotenv = require('dotenv-webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: { main: './src/index.js' },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "[name].[contenthash].js",
-    chunkFilename: "[name].[contenthash].chunk.js",
-    clean: true,
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public/"),
-    },
-    port: 3000,
-    historyApiFallback: true,
-  },
-  plugins: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  const plugins = [
     new Dotenv(),
-    new BundleAnalyzerPlugin({
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    isProduction && new BundleAnalyzerPlugin({
       analyzerMode: 'static',
-      openAnalyzer: false,
+      openAnalyzer: true,
       reportFilename: 'bundle-report.html'
     })
-  ].filter(Boolean),
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\/]node_modules[\/]/,
-          name: 'vendors',
-          chunks: 'all',
+  ].filter(Boolean);
+
+  return {
+    entry: { main: './src/index.js' },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/",
+      filename: "[name].[contenthash].js",
+      chunkFilename: "[name].[contenthash].chunk.js",
+      clean: true,
+    },
+    devServer: {
+      static: path.join(__dirname, "public/"),
+      port: 3000,
+      historyApiFallback: true,
+    },
+    plugins: plugins,
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\/]node_modules[\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
-  performance: {
-    hints: 'warning',
-    maxAssetSize: 512000,
-    maxEntrypointSize: 512000,
-  },
+  };
 };
